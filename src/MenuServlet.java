cbe6c6a291172edf71e316d8be0007e934fa38de
Thery Ehrlich <thery.ehrlich@etu.hesge.ch>
2018-05-01 13:20:53 +0200

    basic links for booklist and connexion done

--- src/MenuServlet.java
+++ src/MenuServlet.java
@@ -8,14 +8,10 @@ import java.util.Map;
 public class MenuServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println(req.getParameter("bt").equals("Livres"));
-        System.out.println(req.getParameter("bt").equals("Connexion"));
-        if (req.getParameter("bt").equals("Livres")) {
-            req.getRequestDispatcher("/WEB-INF/book_list.jsp").forward(req, resp);
-        }
-        if (req.getParameter("bt").equals("Connexion")) {
-            System.out.println("Going to authentication page !");
-            req.getRequestDispatcher("/WEB-INF/authentication_page.jsp").forward(req, resp);
+        switch (req.getParameter("bt")) {
+            case "Livres" : req.getRequestDispatcher("/WEB-INF/book_list.jsp").forward(req, resp); break;
+            case "Connexion" : req.getRequestDispatcher("/WEB-INF/authentication_page.jsp").forward(req, resp); break;
+            default: break;
         }
     }
 }
