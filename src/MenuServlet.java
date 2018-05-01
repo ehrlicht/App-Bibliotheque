360c0dd5e4f4b5581d66fd9583f6e3cc7c258ec6
Thery Ehrlich <thery.ehrlich@etu.hesge.ch>
2018-05-01 11:48:55 +0200

    renamed  in english

--- src/MenuServlet.java
+++ src/MenuServlet.java
@@ -1,6 +1,15 @@
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
 
 public class MenuServlet extends HttpServlet {
-
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Map m = req.getParameterMap();
+        System.out.println(m);
+    }
 }
 
