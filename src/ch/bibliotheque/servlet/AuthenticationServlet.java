0d0db5aeb0eab88b0759fc2b521e9aac4a5566fe
artrit.ajdini <artrit.ajdini@etu.hesge.ch>
2018-05-11 09:55:37 +0200

    Auth MàJ

--- src/ch/bibliotheque/servlet/AuthenticationServlet.java
+++ src/ch/bibliotheque/servlet/AuthenticationServlet.java
@@ -4,17 +4,23 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class AuthenticationServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+
+
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+
+        System.out.println(session.getServletContext());
+        System.out.println(session.getAttributeNames());
         req.getRequestDispatcher("/WEB-INF/authentication_page.jsp").include(req, resp);
     }
 }
