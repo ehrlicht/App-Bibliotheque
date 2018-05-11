b8e9f6ea41ef2f010f90aa3032255f1f84df3b2d
Thery Ehrlich <thery.ehrlich@icloud.com>
2018-05-11 17:21:00 +0200

    Alerte de mauvais mdp

--- src/ch/bibliotheque/dao/DataInitializer.java
+++ src/ch/bibliotheque/dao/DataInitializer.java
@@ -16,7 +16,7 @@ public class DataInitializer implements ServletContextListener {
     private final BookService bs;
 
     @Inject
-    public DataInitializer (BookService bs) {
+    public DataInitializer(BookService bs) {
         this.bs = bs;
     }
 
@@ -26,9 +26,21 @@ public class DataInitializer implements ServletContextListener {
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         Book b1 = null;
         Book b2 = null;
+        Book b3 = null;
+        Book b4 = null;
+        Book b5 = null;
+        Book b6 = null;
+        Book b7 = null;
+        Book b8 = null;
         try {
-            b1 = new Book("Ma vie mon Oeuvre", "Moi", "Jacquette", sdf.parse("01.11.2014"));
-            b2 = new Book("La Bible", "Dieu", "Bigbang Ed.", new Date());
+            b1 = new Book("La Bible", "Dieu", "Bigbang Publications.", new Date());
+            b2 = new Book("Les aventures de Tata au pays du zeub noir", "Hergé", "Editions la Jacquette", sdf.parse("01.11.2014"));
+            b3 = new Book("Les aventures de Tata - On a marché sur ma lune", "Hergé", "Editions la Jacquette", sdf.parse("01.11.2014"));
+            b4 = new Book("Les aventures de Tata - Le cigare du gros Léon", "Hergé", "Editions la Jacquette", sdf.parse("01.11.2014"));
+            b5 = new Book("Les aventures de Tata - Les 2 boules de Christelle", "Hergé", "Editions la Jacquette", sdf.parse("01.11.2014"));
+            b6 = new Book("Les aventures de Tata - Les bijoux de la casse moi le fion", "Hergé", "Editions la Jacquette", sdf.parse("01.11.2014"));
+            b7 = new Book("Les aventures de Tata - Les 2 boules de Christelle", "Hergé", "Editions la Jacquette", sdf.parse("01.11.2014"));
+            b8 = new Book("Les aventures de Tata - Le temple du fist fucking", "Hergé", "Editions la Jacquette", sdf.parse("01.11.2014"));
         } catch (ParseException e) {
             e.printStackTrace();
         }
