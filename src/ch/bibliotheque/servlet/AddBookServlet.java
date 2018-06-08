cca6fa32c7c44e6eb035a1e119df4ad45656eb9a
Thery Ehrlich <thery.ehrlich@etu.hesge.ch>
2018-06-08 09:24:42 +0200

    - changement des modales update et delete en alertes

--- src/ch/bibliotheque/servlet/AddBookServlet.java
+++ src/ch/bibliotheque/servlet/AddBookServlet.java
@@ -23,7 +23,7 @@ public class AddBookServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if (req.isUserInRole("ADMIN")) {
+        if (req.isUserInRole("ADMINAB")) {
             req.setAttribute("errorMessage", false);
             req.getRequestDispatcher("/WEB-INF/add_book.jsp").include(req, resp);
         } else {
